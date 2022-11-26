767adfe6396edffc93257013b22847d2f386e97d
DanielCogiel <danicog433@student.polsl.pl>
2022-11-26 18:27:59 +0100

    modyfikacja settings.py

--- test_django_app/test_django_app/settings.py
+++ test_django_app/test_django_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l8=y=ghws4am37i7xodjjgso67&2a@($im=6#b@&xgkf^#9%+p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dcogiel.pythonanywhere.com']
 
 
 # Application definition
